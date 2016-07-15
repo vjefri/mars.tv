d65994563ac21fc935f7c734d3a685e43541bbda
Jefri Vanegas <vjefri@gmail.com>
2016-07-14 22:41:08 -0700

    fix guide-scroller

---
+++ client/dist/0.99463a18867d78f0c21e.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(0,{
+
+/***/ 294:
+/***/ function(module, exports) {
+
+	eval("// removed by extract-text-webpack-plugin//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMjk0LmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vY2xpZW50L3NyYy9zdHlsZXMvbWFpbi5zY3NzPzdiNmQiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vKioqKioqKioqKioqKioqKipcbiAqKiBXRUJQQUNLIEZPT1RFUlxuICoqIC4vY2xpZW50L3NyYy9zdHlsZXMvbWFpbi5zY3NzXG4gKiogbW9kdWxlIGlkID0gMjk0XG4gKiogbW9kdWxlIGNodW5rcyA9IDBcbiAqKi8iXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ }
+
+})
--- client/src/reducers/main.js
+++ client/src/reducers/main.js
@@ -1,11 +1,1 @@
-import * as ACTIONS from '../constants/player';
-// import _ from 'lodash'
 
-const INITIAL_STATE = {};
-
-export default (state = INITIAL_STATE , action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
